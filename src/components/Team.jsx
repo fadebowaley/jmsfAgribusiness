4b404525b398882a014340c9d35baeea71ab1c02
Ademola Adebowale <fadebowaley@gmail.com>
2026-07-10 20:49:54 +0100

    update team grid to 4-column layout

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -34,7 +34,7 @@ const Team = () => {
         </div>
 
         {/* Team Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mb-16 justify-items-center">
           {teamMembers.map((member) => (
             <div
               key={member.id}
